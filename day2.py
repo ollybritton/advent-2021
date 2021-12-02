3f000e95af44124e83878441c5aab2342a56fe1d
Olly Britton <ollybritton@gmail.com>
2021-12-02 15:00:42 +0000

    day 2

---
+++ day2.py
@@ -0,0 +1,51 @@
+from utils import parse_puzzle_input
+
+day = 2
+
+def parse_line(line):
+    line = line.split(" ")
+    return line[0], int(line[1])
+
+def part1():
+    data = parse_puzzle_input(day, 1, "\n", parse_line)
+    
+    horizontal = 0
+    depth = 0
+
+    for direction, magnitude in data:
+        if direction == "forward":
+            horizontal += magnitude
+        elif direction == "up":
+            depth -= magnitude
+        else:
+            depth += magnitude
+
+    return horizontal * depth
+
+def part2():
+    data = parse_puzzle_input(day, 1, "\n", parse_line)
+    
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for direction, magnitude in data:
+        if direction == "forward":
+            horizontal += magnitude
+            depth += aim * magnitude
+        elif direction == "up":
+            aim -= magnitude
+        elif direction == "down":
+            aim += magnitude
+
+
+    return horizontal * depth
+
+answers = [
+    [part1, 1690020],
+    [part2, 1408487760],
+]
+
+if __name__ == "__main__":
+    print("Part 1:", part1())
+    print("Part 2:", part2())
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import time
 
 import day1
+import day2
 
 # answers are a list of [[func, official], [func, official]]
 def verify(day, answers):
+    now = time.time()
+
     for i, pair in enumerate(answers):
         got = pair[0]()
         expected = pair[1]
         print(f"Day {day}.{i+1}: got {got}, expected {expected} {'✅' if got == expected else '❌'}")
 
+    print(f"Time taken: {round((time.time() - now)*10000)/10}ms")
+
 if __name__ == "__main__":
     verify(1, day1.answers)
+    verify(2, day2.answers)
